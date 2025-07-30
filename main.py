0bae6d4383a96f54858f3d64b7ae58e6068df6ec
harishkarthiktk <45687759+harishkarthiktk@users.noreply.github.com>
2025-07-30 12:13:06 +0530

    Branch test.

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from pprint import pprint
 def greet_all(names):
     for name in names:
         print(f"Hello, {name}!")
+
+def export_names(names):
+    names_list = []
+    for name in names:
+        names_list.append(name)
+    return(names_list)
+
 if __name__ == "__main__":
     people = ["Alice", "Bob", "Charlie"]
-    pprint(people)
+    # pprint(people)
     greet_all(people)
+    pprint(export_names(people))
