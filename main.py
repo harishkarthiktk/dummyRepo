21fb7ec24af9e514597cfb805196245b923bbf55
harishkarthiktk <45687759+harishkarthiktk@users.noreply.github.com>
2025-07-30 11:43:11 +0530

    Third commit, cleaning lines.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from pprint import pprint
-
 def greet_all(names):
     for name in names:
         print(f"Hello, {name}!")
-
 if __name__ == "__main__":
     people = ["Alice", "Bob", "Charlie"]
     pprint(people)
     greet_all(people)
-
